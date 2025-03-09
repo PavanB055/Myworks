295e29c7fe05fd85bd075afd1295b553655c24cb
PavanB055 <2022pavan.na@vidyashilp.edu.in>
2025-03-09 17:41:56 +0530

    Update E5.js

--- E5/E5.js
+++ E5/E5.js
@@ -1,15 +1,66 @@
-document.getElementById("textInput").addEventListener("input", function() {
-  document.getElementById("styledDiv").textContent = this.value || "Hello!";
-});
 
-document.getElementById("borderInput").addEventListener("input", function() {
-  document.getElementById("styledDiv").style.borderWidth = this.value + "px";
-});
+let box1 = document.getElementById('box1')
 
-document.getElementById("widthSlider").addEventListener("input", function() {
-  document.getElementById("styledDiv").style.width = this.value + "px";
-});
+//Text Input ------------------------------
 
-document.getElementById("fontSelect").addEventListener("change", function() {
-  document.getElementById("styledDiv").style.fontFamily = this.value;
-});
+let textInput = document.getElementById('textInput1');
+
+function displayTextValue() {
+    box1.innerText = textInput.value;
+}
+
+//Number Input ------------------------------
+
+let numInput = document.getElementById('numberInput1');
+
+function numberValue() {
+    box1.style.border = numInput.value + "px solid black"
+}
+
+
+//Radio Buttons ------------------------------
+
+let serif = document.getElementById('radioSerif');
+function changeFontSerif() {
+    box1.style.fontFamily = serif.value;
+}
+
+let sans = document.getElementById('radioSans');
+function changeFontSans() {
+    box1.style.fontFamily = sans.value;
+}
+
+let mono = document.getElementById('radioMono');
+function changeFontMono() {
+    box1.style.fontFamily = mono.value;
+}
+
+
+//Slider Text Size ------------------------------
+
+let sizeSlider = document.getElementById('slider');
+
+function adjustSize() {
+    box1.style.fontSize = sizeSlider.value + "px"
+}
+
+
+//Slider RGB ------------------------------
+
+function adjustColor() {
+
+let R = document.getElementById('R').value;
+let G = document.getElementById('G').value;
+let B = document.getElementById('G').value;
+
+box1.style.backgroundColor = "rgb(" + R + "," + G + "," + B + ")";
+
+}
+
+
+//Dropdown Colours ------------------------------
+
+function chooseColor() {
+    let dropdown = document.getElementById('dropdown');
+    box1.style.backgroundColor = dropdown.value;
+}
